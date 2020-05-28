eca596f55299584587f3ae8d3936a2c70ab4c6d8
amitgade <amitgaade@gmail.com>
2020-05-28 19:00:24 +0530

    action-Incorporate MVC architecture : Use Servlets with JSPs

--- src/main/java/com/smartcode/servlet/MenuSearchServlet.java
+++ src/main/java/com/smartcode/servlet/MenuSearchServlet.java
@@ -1,9 +1,11 @@
 package com.smartcode.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,30 +18,18 @@ import com.smartcode.domain.MenuItem;
 @WebServlet("/searchResults.html")
 public class MenuSearchServlet extends HttpServlet {
 
-	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-		PrintWriter out = resp.getWriter();
-		resp.setContentType("text/html");
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
 		String searchTerm = req.getParameter("searchTerm");
 
 		MenuDao menuDao = MenuDaoFactory.getMenuDao();
 		List<MenuItem> menuItems = menuDao.find(searchTerm);
 
-		if (menuItems.size() > 0) {
-			out.println("<html><body><h1>Modern Kafe</h1>");
-			out.println("<h2>Dishes containing " + searchTerm + "</h2><ul>");
-			for (MenuItem menuItem : menuItems) {
-				out.println("<li>" + menuItem + " " + menuItem.getDescription() + "</li>");
-			}
-			out.println("</ul></body></html>");
-
-		} else {
-			out.println("<html><body><h1>Modern Kafe</h1>");
-			out.println("<p>I'm sorry, there are no dishes containing  " + searchTerm);
-			out.println("</p></body></html>");
-		}
-
-		out.close();
+		req.setAttribute("menuItems", menuItems);
+		
+		// we are done with business logic; now hand it over to jsp for display
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatch = context.getRequestDispatcher("/searchResults.jsp");
+		dispatch.forward(req, resp);
 	}
 }
